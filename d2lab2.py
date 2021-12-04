2155b9c79ca2eba1b4c4bb4cf634da0a7103d6c0
rozmaharjan <rox.maharjan@gmail.com>
2021-12-04 21:53:31 +0545

    adding day 2 lab work

---
+++ d2lab2.py
@@ -0,0 +1,6 @@
+#find BMI of a person where take mass and height as input from the user
+h=int(input("enter yout height in m: "))
+m=int(input("enter yout mass in kg: "))
+BMI=m/h*2
+print("the bmi of a person is {} kg/m\u00b2". format(BMI))
+
